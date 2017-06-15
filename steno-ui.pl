05bfb78425c1d17f10552d6c6e83f67f3b634988
Henry Hirsch <henry@w3-net.de>
2017-06-15 08:43:30 +0200

    Add close command

--- steno-ui.pl
+++ steno-ui.pl
@@ -25,12 +25,23 @@ my $filename = 'test.json';
 my $newUpdateTime;
 my $lastUpdateTime  = (stat($filename)->mtime);
 notify( $mw, "Steno UI started", 2000 );
+my $ui = $mw->Toplevel();
+initUi( $ui, $mw);
 $mw->repeat(
     2000,
     sub {
         $newUpdateTime  = (stat($filename)->mtime);
         if ($newUpdateTime > $lastUpdateTime) {
-            notify( $mw, "$json->{'mode'} $lastUpdateTime", 2000 );
+            notify( $mw, "Mode $json->{'mode'} $lastUpdateTime", 2000 );
+            if ($json->{'command'} eq "close"){
+                $ui->destroy;
+                $mw->destroy;
+            }
+            if ($json->{'command'} eq "hide"){
+            }
+            if ($json->{'command'} eq "show"){
+            }
+
             $lastUpdateTime  = $newUpdateTime;
         }
     }
@@ -44,13 +55,18 @@ sub notify {
     $notification->transient($mw);
     $notification->overrideredirect(1);
     $notification->Popup( -popanchor => 'c' );
-    $notification->geometry("-0+20");
+    $notification->geometry("-0+60");
     my $frame = $notification->Frame( -border => 5, -relief => 'groove' )->pack;
     $frame->Label( -text => $message, )->pack( -padx => 5 );
-    $frame->Button(
-        -text    => 'OK',
-        -command => sub { $notification->destroy; undef $notification 
-        },
-        )->pack( -pady => 5 );
     $notification->after( $ms, sub { $notification->destroy } );
+}
+
+sub initUi {
+    my ( $ui, $mw ) = @_;
+    $ui->transient($mw);
+    $ui->overrideredirect(1);
+    $ui->Popup( -popanchor => 'c' );
+    $ui->geometry("-0+20");
+    my $frame = $ui->Frame( -border => 5, -relief => 'groove' )->pack;
+    $frame->Label( -text => "Status", )->pack( -padx => 5 );
 }
